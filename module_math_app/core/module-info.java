dbd55342834068ca7a6cb5cb302160eb7c896215
Jashon Menya <jjmenya14@gmail.com>
2023-05-12 16:58:53 -0500

    modules complete

---
+++ module_math_app/core/module-info.java
@@ -0,0 +1,3 @@
+module core {
+    exports com.example.math;
+}
---
+++ module_math_app/core/src/com/example/math/MathUtils.java
@@ -0,0 +1,39 @@
+package com.example.math;
+
+public class MathUtils {
+    /**
+     * @param a
+     * @param b
+     * @return sum of the parameters
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * @param a
+     * @param b
+     * @return difference of parameters
+     */
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    /**
+     * @param a
+     * @param b
+     * @return multiplication of parameters
+     */
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * @param a
+     * @param b
+     * @return division of parameters
+     */
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
