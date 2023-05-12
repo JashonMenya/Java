1c7d586deeb86532fd72581c2796382e97bbc208
Jashon Menya <jjmenya14@gmail.com>
2023-05-12 18:29:39 -0500

    linting package info

--- module_math_app/client/src/com/example/client/package-info.java
+++ module_math_app/client/src/com/example/client/package-info.java
@@ -1,5 +1,6 @@
-package com.example.client;
 /**
  * This package contains the client classes for the Math App module.
- * It provides the necessary functionality to interact with the server-side components.
+ * It provides the necessary functionality to interact with the server-side
+ * components.
  */
+package com.example.client;
--- module_math_app/core/src/com/example/math/package-info.java
+++ module_math_app/core/src/com/example/math/package-info.java
@@ -1,5 +1,5 @@
-package com.example.math;
 /**
  * This package contains the Utility classes for the Math App module.
  * It provides the necessary functionality to perform mathematical operations.
  */
+package com.example.math;
