471173a6973317d5fd97b6628a5604bdded52fc6
Jashon Menya <jjmenya14@gmail.com>
2023-05-12 17:39:34 -0500

    linting client app

--- module_math_app/client/src/com/example/client/ClientApp.java
+++ module_math_app/client/src/com/example/client/ClientApp.java
@@ -3,22 +3,22 @@ package com.example.client;
 import com.example.math.MathUtils;
 
 public class ClientApp {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
 
         // Addition
         int addition = MathUtils.add(2, 2);
-        System.out.println("Results: "+ addition);
+        System.out.println("Results: " + addition);
 
         //Subtraction
         int subtraction = MathUtils.subtract(8, 4);
-        System.out.println("Results: "+ subtraction);
+        System.out.println("Results: " + subtraction);
 
         // Multiplication
         int multiply = MathUtils.multiply(2, 2);
-        System.out.println("Results: "+ multiply);
+        System.out.println("Results: " + multiply);
 
         // Divide
         int divide = MathUtils.divide(8, 2);
-        System.out.println("Results: "+ divide);
+        System.out.println("Results: " + divide);
     }
 }
