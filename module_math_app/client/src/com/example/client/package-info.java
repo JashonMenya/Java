17a5718fd22e8198acf2cb220cc4ce9826340267
Jashon Menya <jjmenya14@gmail.com>
2023-05-12 18:24:14 -0500

    adding package infos

---
+++ module_math_app/client/src/com/example/client/package-info.java
@@ -0,0 +1,5 @@
+package com.example.client;
+/**
+ * This package contains the client classes for the Math App module.
+ * It provides the necessary functionality to interact with the server-side components.
+ */
---
+++ module_math_app/core/src/com/example/math/package-info.java
@@ -0,0 +1,5 @@
+package com.example.math;
+/**
+ * This package contains the Utility classes for the Math App module.
+ * It provides the necessary functionality to perform mathematical operations.
+ */
