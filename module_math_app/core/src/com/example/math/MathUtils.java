8c56b5d983b21a3ecc222eaa2b9ada77b0dfb0eb
Jashon Menya <jjmenya14@gmail.com>
2023-05-12 18:04:25 -0500

    math utils lint

--- module_math_app/core/src/com/example/math/MathUtils.java
+++ module_math_app/core/src/com/example/math/MathUtils.java
@@ -1,39 +1,56 @@
 package com.example.math;
 
-public class MathUtils {
+public final class MathUtils {
+
+    private MathUtils() {
+        throw new UnsupportedOperationException("MathUtils class cannot be instantiated.");
+    }
+
     /**
-     * @param a
-     * @param b
-     * @return sum of the parameters
+     * Adds two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The sum of the two numbers.
      */
-    public static int add(int a, int b) {
+    public static int add(final int a, final int b) {
         return a + b;
     }
 
     /**
-     * @param a
-     * @param b
-     * @return difference of parameters
+     * Subtracts two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The difference between the two numbers.
      */
-    public static int subtract(int a, int b) {
+    public static int subtract(final int a, final int b) {
         return a - b;
     }
 
     /**
-     * @param a
-     * @param b
-     * @return multiplication of parameters
+     * Multiplies two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The product of the two numbers.
      */
-    public static int multiply(int a, int b) {
+    public static int multiply(final int a, final int b) {
         return a * b;
     }
 
     /**
-     * @param a
-     * @param b
-     * @return division of parameters
+     * Divides two numbers.
+     *
+     * @param a The numerator.
+     * @param b The denominator.
+     * @return The division result.
+     * @throws ArithmeticException If the denominator is zero.
      */
-    public static int divide(int a, int b) {
+    public static int divide(final int a, final int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
         return a / b;
     }
 }
