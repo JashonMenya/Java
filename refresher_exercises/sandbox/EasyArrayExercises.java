06ea13bf615b7c5705137a5b504395bc60339dfc
Steve Menya <jjmenya14@gmail.com>
2025-06-18 15:07:12 -0500

    longest consercutive count of elements in an array

--- refresher_exercises/sandbox/EasyArrayExercises.java
+++ refresher_exercises/sandbox/EasyArrayExercises.java
@@ -140,4 +140,52 @@ public class EasyArrayExercises {
         return Math.max(allPositiveReturn, negativesIncluded);
     }
 
+    /**
+     *
+     * Given an array arr[] consisting of only 0's and 1's, the task is to find the
+     * count of a maximum number of consecutive 1's or 0's present in the array.
+     *
+     * Examples :
+     *
+     * Input: arr[] = {1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1}
+     * Output: 4
+     * Explanation: The maximum number of consecutive 1's in the array is 4 from
+     * index 8-11.
+     *
+     * Input: arr[] = {0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
+     * Output: 2
+     * Explanation: The maximum number of consecutive 0's in the array is 2 from
+     * index 0-1.
+     *
+     * Input: arr[] = {0, 0, 0, 0}
+     * Output: 4
+     * Explanation: The maximum number of consecutive 0's in the array is 4.
+     */
+
+    public String maxConsecutive(int[] input) {
+        int counter = 0;
+        int highestCount = 0;
+        int currentElement = -1;
+        int winningElementValue = -1;
+
+        // 1
+        for (int number : input) {
+            if (number == currentElement) {
+                counter += 1;
+            } else {
+                counter = 1;
+                currentElement = number;
+            }
+
+            // next logic
+            if (counter > highestCount) {
+                highestCount += 1;
+                winningElementValue = number;
+            }
+
+        }
+
+        return "The maximum number of consecutive " + winningElementValue + "'s in the array is:" + highestCount;
+    }
+
 }
