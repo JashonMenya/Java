5082b0f61d1fd12cdbcf8ea7973038dd4f82c04f
Steve Menya <jjmenya14@gmail.com>
2025-07-03 15:50:57 -0500

    user profile

--- refresher_exercises/sandbox/Main.java
+++ refresher_exercises/sandbox/Main.java
@@ -1,4 +1,43 @@
 package refresher_exercises.sandbox;
 
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // Get User Input
+
+        Scanner scanner = new Scanner(System.in);
+        List<UserProfile> userProfiles = new ArrayList<>();
+
+        int profilesToCreate = 2;
+        while (profilesToCreate > 0) {
+            userProfiles.add(collectUserData(scanner));
+            profilesToCreate--;
+        }
+        System.out.println(userProfiles);
+
+        // Cheking age status
+        for (UserProfile userprofile : userProfiles) {
+            System.out.println(userprofile);
+        }
+
+    }
+
+    public static UserProfile collectUserData(Scanner scanner) {
+        System.out.println("What's your First Name");
+        String firstName = scanner.nextLine();
+        System.out.println("What's your Last Name");
+        String lastName = scanner.nextLine();
+        System.out.println("What's your email");
+        String email = scanner.nextLine();
+        System.out.println("What's your age");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        return new UserProfile(firstName, lastName, email, age);
+    }
+
 }
---
+++ refresher_exercises/sandbox/UserProfile.java
@@ -0,0 +1,66 @@
+package refresher_exercises.sandbox;
+
+public class UserProfile {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public UserProfile(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String isAdult() {
+        if (this.age >= 18) {
+            return "Adult";
+        } else {
+            return "Minor";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", Adult Status=" + isAdult() +
+                '}';
+    }
+}
