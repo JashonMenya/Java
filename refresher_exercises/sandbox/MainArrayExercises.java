dfef62e1e0f6501c5e17785653b2af18da60d30e
JashonMenya <jjmenya14@gmail.com>
2025-07-08 16:01:36 -0500

    array excersises main

--- refresher_exercises/sandbox/MainArrayExercises.java
+++ refresher_exercises/sandbox/MainArrayExercises.java
@@ -1,4 +1,16 @@
 package refresher_exercises.sandbox;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainArrayExercises {
+    public static void main(String[] args) {
+        EasyArrayExercises easyArrayExercises = new EasyArrayExercises();
+        List<Integer> listInt = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        int[] sampleArray = {1, 2, 3, 4, 5};
+
+        int[] result = easyArrayExercises.rotateArr(sampleArray, 2);
+        System.out.println((Arrays.toString(result)));
+    }
 }
