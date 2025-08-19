08198edb17a87a314c8053fd64d2259be79c51f0
JashonMenya <jjmenya14@gmail.com>
2025-08-18 19:29:05 -0500

    interfaces main

--- refresher_exercises/code_challenges/shirt_product/MainSp.java
+++ refresher_exercises/code_challenges/shirt_product/MainSp.java
@@ -1,4 +1,19 @@
 package refresher_exercises.code_challenges.shirt_product;
 
 public class MainSp {
+    public static void main(String[] args) {
+        String name = "Golf Shirt";
+        double price = 49.95;
+        String color = "White";
+        String size = "Medium";
+        Shirt shirt = buyShirt(name, price, size, color);
+
+        System.out.println(shirt);
+
+    }
+
+    // Do not change this method
+    static Shirt buyShirt(String name, double price, String size, String color) {
+        return new Shirt(name, price, size, color);
+    }
 }
